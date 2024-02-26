013bd482239454e37904a8dca054b9049d075283
shruthika1216 <157671920+shruthika1216@users.noreply.github.com>
2024-02-26 10:11:13 -0600

    Update my_first_dbt_model.sql

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -15,18 +15,10 @@ with source_data as (
     union all
     select null as id
     select 2 as id
-    
-        
+          
    
-
 
 )
 
 select *
 from source_data
-
-/*
-    Uncomment the line below to remove records with null id values
-*/
-
--- where id is not null
