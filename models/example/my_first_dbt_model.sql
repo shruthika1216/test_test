bb00503488862dc740c19dc31fa989b51b9136ab
savitha dbt cloud <saviamar16@gmail.com>
2024-02-27 01:20:26 +0000

    ch

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,34 +1,24 @@
 
-{{ config(materialized='table',
-
-) }}
-
-
+{{ config(materialized='table') }}
 
 with source_data as (
 
     select 1 as id
     union all
-<<<<<<< HEAD
-    select null as id
-    union all
-    select null as id
-    union all
-    select null as id
-=======
     select 2 as id
     
         
    
->>>>>>> 23e6edf425cac2ddb660a1d0c370c8bb46b63962
+
+)
+
+select *
+from source_data
+   
+
 
 )
 
 select *
 from source_data
 
-/*
-    Uncomment the line below to remove records with null id values
-*/
-
--- where id is not null
